3a9d27f72d83c1584b85cbf46b1201ca5f901983
Mehmet Noyan AYDIN <Noyan@Mehmets-MacBook-Pro.local>
2016-02-06 17:51:21 -0700

    Changed Application Name because this is a simple hello world application will add reddit clone as a seperate application

--- Basic-HelloWorldApp/app.ts
+++ Basic-HelloWorldApp/app.ts
@@ -1,19 +1,21 @@
 import { bootstrap } from "angular2/platform/browser";
 import { Component } from "angular2/core";
+import { NgFor } from "angular2/common";
 
 @Component({
 	selector: 'hello-world',
 	template: `
-	<div>
-		Hello World {{name}}
-	</div>
+	<ul>
+        <li *ngFor="#name of names">Hello {{ name }}</li>
+    </ul>
 	`
 })
 
 class HelloWorld {
-	name:string;
+	names:string[];
+    
 	constructor(){
-		this.name = 'Noyan';
+		this.names = ['Noyan', 'Burcu', 'Britney', 'Olie'];
 	}
 }
 
